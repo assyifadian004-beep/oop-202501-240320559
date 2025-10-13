b522a1237e90cefd119865693c7d69b8e24d53cc
As Syifa Dian R <assyifadian004@gmail.com>
2025-10-13 15:06:23 +0700

    week1-hello-pos

---
+++ praktikum/week1-setup-hello-pos/src/main/HelloFunctional.java
@@ -0,0 +1,9 @@
+public class HelloFunctional {
+    public static void main(String[] args) {
+        tampilkanPesan();
+    }
+    static void tampilkanPesan() {
+        System.out.println("Hello World, I am Dian Nur Safitri-240320563");
+    }
+    
+}
